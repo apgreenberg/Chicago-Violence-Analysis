d4187b791e056618510317159af98c4cae98c50c
apgreenberg <43008006+apgreenberg@users.noreply.github.com>
2020-07-31 09:59:35 -0400

    yeah idk

---
+++ toplevdata.js
@@ -0,0 +1,57 @@
+origData = [
+  {
+    name: "police",
+    values: [
+      {year: "2012", percent: "0"},
+      {year: "2013", percent: "-0.5"},
+      {year: "2014", percent: "2.6"},
+      {year: "2015", percent: "6"},
+      {year: "2016", percent: "0.1"},
+      {year: "2017", percent: "2.2"},
+      {year: "2018", percent: "2.9"},
+      {year: "2019", percent: "4.7"},
+      {year: "2020", percent: "4"},
+    ]
+  },
+  {
+    name: "violent",
+    values: [
+      {year: "2012", percent: "0"},
+      {year: "2013", percent: "-11.8"},
+      {year: "2014", percent: "10.6"},
+      {year: "2015", percent: "-2"},
+      {year: "2016", percent: "7.7"},
+      {year: "2017", percent: "-2"},
+      {year: "2018", percent: "-1.9"},
+      {year: "2019", percent: "-4.3"},
+      {year: "2020", percent: "-7.5"},
+    ]
+  },
+  {
+    name: "other",
+    values: [
+      {year: "2012", percent: "0"},
+      {year: "2013", percent: "-8.9"},
+      {year: "2014", percent: "-12.4"},
+      {year: "2015", percent: "-7.6"},
+      {year: "2016", percent: "-15.7"},
+      {year: "2017", percent: "-8.2"},
+      {year: "2018", percent: "1.5"},
+      {year: "2019", percent: "-0.6"},
+      {year: "2020", percent: "-12.9"},
+    ]
+  },
+  {
+    name: "social",
+    values: [
+      {year: "2012", percent: "0"},
+      {year: "2013", percent: "-40.4"},
+      {year: "2014", percent: "28.8"},
+      {year: "2015", percent: "1.4"},
+      {year: "2016", percent: "-4.2"},
+      {year: "2017", percent: "5.7"},
+      {year: "2018", percent: "-2.3"},
+      {year: "2019", percent: "-7.7"},
+      {year: "2020", percent: "45.7"},
+    ]
+  }]
